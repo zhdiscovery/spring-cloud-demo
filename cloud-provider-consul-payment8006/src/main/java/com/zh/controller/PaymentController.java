47b91ff9dc24e144060b8fe5c08016b80e95789d
zh <277708272@qq.com>
2020-05-27 16:07:14 +0800

    add consul module

---
+++ cloud-provider-consul-payment8006/src/main/java/com/zh/controller/PaymentController.java
@@ -0,0 +1,20 @@
+package com.zh.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping("/payment/consul")
+    public String paymentzk() {
+        return "spring cloud with consul: " + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+
+}
