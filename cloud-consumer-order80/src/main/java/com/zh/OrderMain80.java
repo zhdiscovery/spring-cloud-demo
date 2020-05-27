32f54779531ced686c3fb4596b29c29f9c0f5aad
zh <277708272@qq.com>
2020-05-27 16:55:08 +0800

    add ribbon module (unfinished)

--- cloud-consumer-order80/src/main/java/com/zh/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/zh/OrderMain80.java
@@ -1,11 +1,15 @@
 package com.zh;
 
+import com.myrule.MyRuleConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+//name中的服务名要大写，小写没有效果
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MyRuleConfig.class)//开启客户端负载均衡
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
--- cloud-consumer-order80/src/main/java/com/zh/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/zh/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.zh.controller;
 import com.zh.entities.CommonResult;
 import com.zh.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,7 @@ import javax.annotation.Resource;
 @Slf4j
 public class OrderController {
 
-//    public static final String PAYMENT_URL = "http://localhost:8001";
+    //    public static final String PAYMENT_URL = "http://localhost:8001";
     public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";//集群版,使用eureka上注册的服务名,小写也可以识别
 
     @Resource
@@ -28,6 +29,16 @@ public class OrderController {
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> get(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            return entity.getBody();
+        } else {
+            return new CommonResult<>(404, "操作失败！");
+        }
     }
 
 }
