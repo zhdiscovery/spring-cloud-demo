c27127534f922658126db91f33b9d13fdb6aaa6e
zh <277708272@qq.com>
2020-05-27 10:41:18 +0800

    add eureka7002 module

--- cloud-provider-payment8001/src/main/java/com/zh/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/zh/PaymentMain8001.java
@@ -2,8 +2,10 @@ package com.zh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient//启动eureka客户端
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
