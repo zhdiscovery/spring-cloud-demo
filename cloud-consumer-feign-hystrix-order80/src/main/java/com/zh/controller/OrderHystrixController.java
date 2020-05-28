59465a2fe17e0e68b939ba29ad53359fa6d5892e
zh <277708272@qq.com>
2020-05-28 17:24:42 +0800

    add client default fallback method

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/zh/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/zh/controller/OrderHystrixController.java
@@ -1,5 +1,6 @@
 package com.zh.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.zh.service.PaymentHystrixService;
@@ -9,10 +10,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.concurrent.TimeUnit;
 
 @RestController
 @Slf4j
+@DefaultProperties(defaultFallback = "payment_global_fallback_method")
 public class OrderHystrixController {
 
     @Resource
@@ -26,16 +27,22 @@ public class OrderHystrixController {
     }
 
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
-    @HystrixCommand(fallbackMethod = "paymentInfo_TimeoutHandler", commandProperties = {
-        @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "4000")
-    })
+//    @HystrixCommand(fallbackMethod = "paymentInfo_TimeoutHandler", commandProperties = {
+//        @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "4000")
+//    })
+    @HystrixCommand
     public String paymentInfo_Timeout(@PathVariable("id") Integer id) {
+//        int err = 1 / 0;
         String result = paymentHystrixService.paymentInfo_Timeout(id);
         return result;
     }
 
     public String paymentInfo_TimeoutHandler(Integer id) {
-        return "我是80消费者，对方支付系统繁忙，请稍后再试";
+        return "我是80消费者，对方支付系统繁忙或自己出错，请稍后再试";
+    }
+
+    public String payment_global_fallback_method() {
+        return "global异常处理，请稍后再试。";
     }
 
 }
