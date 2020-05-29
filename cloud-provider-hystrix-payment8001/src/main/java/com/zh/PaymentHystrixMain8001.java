296c81268318dbd734819a4c65a767c6c9b1b6fa
zh <277708272@qq.com>
2020-05-29 12:00:05 +0800

    add hystrix dashboard

--- cloud-provider-hystrix-payment8001/src/main/java/com/zh/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/zh/PaymentHystrixMain8001.java
@@ -1,9 +1,12 @@
 package com.zh;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,4 +15,19 @@ public class PaymentHystrixMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentHystrixMain8001.class, args);
     }
+
+    /**
+     * 新版本Hystrix需要在主启动类MainAppHystrix8001中指定监控路径
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+
 }
