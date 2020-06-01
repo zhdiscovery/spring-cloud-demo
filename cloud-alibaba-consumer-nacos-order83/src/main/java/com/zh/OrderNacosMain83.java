ca0f46b9bde1c9926ad69cfa712b0c9c7bf2cb0f
zh <277708272@qq.com>
2020-06-01 21:55:44 +0800

    add nacos module,unfinished

---
+++ cloud-alibaba-consumer-nacos-order83/src/main/java/com/zh/OrderNacosMain83.java
@@ -0,0 +1,11 @@
+package com.zh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderNacosMain83 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderNacosMain83.class, args);
+    }
+}
